8f8f816785dc9f652768771f32ca9f9e3f601fbf
Jhonny Moreira <jhonnymoreira@protonmail.com>
2024-05-13 20:50:54 -0300

    chore: add `showHelp` utility

---
+++ src/utilities/index.ts
@@ -0,0 +1,4 @@
+export { fetchTemperature } from './fetch-temperature.js';
+export { getScore } from './get-score.js';
+export { normalizeString } from './normalize-string.js';
+export { showHelp } from './show-help.js';
